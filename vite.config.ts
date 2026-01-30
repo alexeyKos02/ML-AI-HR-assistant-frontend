f9788d829fd74c3b6691a10e2cf6cf2c57d1062b
alexeyKos02 <korolev_al149@mail.ru>
2026-01-30 20:39:50 +0300

    add: fix base name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'ML-AI-HR-assistant-frontend',
   plugins: [vue()],
   resolve: {
     alias: {
